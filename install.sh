db6c99f8a2a3b703583ec2a96e45b991b9708e45
dnc0 <marlonslbr@gmail.com>
2020-04-03 23:26:00 -0300

    install v0.2

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 newversion=$(date +%d/%m/%Y-%H%M)
 
+function copy(){
+	cp -v vimrc $HOME/.vimrc
+	cp -v tmux.conf $HOME/.tmux.conf
+	cp -v zshrc $HOME/.zshrc
+	cp -v -r openbox $HOME/.config
+	cp -v -r konsole $HOME/.local/share
+	cp -v konsolerc $HOME/.config
+	cp -v conky.conf $HOME/.config/conk
+}
 
 #Inicio da execução
-cp -v vimrc $HOME/.vimrc
-cp -v tmux.conf $HOME/.tmux.conf
+
+copy
